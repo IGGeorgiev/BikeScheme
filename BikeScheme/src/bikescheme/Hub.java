b3ccdf5cec3d6dfaa1608db3c754d9b4871cec1d
ivoGen <ivaylo.genev95@gmail.com>
2015-11-28 12:18:39 +0000

    test commit

--- BikeScheme/src/bikescheme/Hub.java
+++ BikeScheme/src/bikescheme/Hub.java
@@ -10,112 +10,103 @@ import java.util.Map;
 import java.util.logging.Logger;
 
 /**
- *  
+ * 
  * Hub system.
- *
+ * 
  * 
  * @author pbj
- *
+ * 
  */
 public class Hub implements AddDStationObserver {
-    public static final Logger logger = Logger.getLogger("bikescheme");
+	public static final Logger logger = Logger.getLogger("bikescheme");
 
-    private HubTerminal terminal;
-    private HubDisplay display;
-    private Map<String,DStation> dockingStationMap;
-    
-    /**
-     * 
-     * Construct a hub system with an operator terminal, a wall display 
-     * and connections to a number of docking stations (initially 0). 
-     * 
-     * Schedule update of the hub wall display every 5 minutes with
-     * docking station occupancy data.
-     * 
-     * @param instanceName
-     */
-    public Hub() {
+	private HubTerminal terminal;
+	private HubDisplay display;
+	private Map<String, DStation> dockingStationMap;
 
-        // Construct and make connections with interface devices
-        terminal = new HubTerminal("ht");
-        terminal.setObserver(this);
-        display = new HubDisplay("hd");
-        dockingStationMap = new HashMap<String,DStation>();
-        
-        // Schedule timed notification for generating updates of 
-        // hub display. 
+	/**
+	 * 
+	 * Construct a hub system with an operator terminal, a wall display and
+	 * connections to a number of docking stations (initially 0).
+	 * 
+	 * Schedule update of the hub wall display every 5 minutes with docking
+	 * station occupancy data.
+	 * 
+	 * @param instanceName
+	 */
+	public Hub() {
+	    //vanka vij me
+		// Construct and make connections with interface devices
+		terminal = new HubTerminal("ht");
+		terminal.setObserver(this);
+		display = new HubDisplay("hd");
+		dockingStationMap = new HashMap<String, DStation>();
 
-        // The idiom of an anonymous class is used here, to make it easy
-        // for hub code to process multiple timed notification, if needed.
-         
-        Clock.getInstance().scheduleNotification(
-                new TimedNotificationObserver() {
+		// Schedule timed notification for generating updates of
+		// hub display.
 
-                    /** 
-                     * Generate dummy display of station occupancy data.
-                     */
-                    @Override
-                    public void processTimedNotification() {
-                        logger.fine("");
+		// The idiom of an anonymous class is used here, to make it easy
+		// for hub code to process multiple timed notification, if needed.
 
-                        String[] occupancyArray = 
-                                // "DSName","East","North","Status","#Occupied","#DPoints"
-                            {  "A",      "100",  "200",  "HIGH",       "19",     "20",
-                               "B",      "300", "-500",   "LOW",        "1",     "50" };
+		Clock.getInstance().scheduleNotification(
+				new TimedNotificationObserver() {
 
-                        List<String> occupancyData = Arrays.asList(occupancyArray);
-                        display.showOccupancy(occupancyData);
-                    }
+					/**
+					 * Generate dummy display of station occupancy data.
+					 */
+					@Override
+					public void processTimedNotification() {
+						logger.fine("");
 
-                },
-                Clock.getStartDate(), 
-                0, 
-                5);
+						String[] occupancyArray =
+						// "DSName","East","North","Status","#Occupied","#DPoints"
+						{ "A", "100", "200", "HIGH", "19", "20", "B", "300",
+								"-500", "LOW", "1", "50" };
 
-    }
+						List<String> occupancyData = Arrays.asList(occupancyArray);
+						display.showOccupancy(occupancyData);
+					}
 
-    public void setDistributor(EventDistributor d) {
-        
-        // The clock device is connected to the EventDistributor here, even
-        // though the clock object is not constructed here, 
-        // as no distributor is available to the Clock constructor.
-        Clock.getInstance().addDistributorLinks(d);
-        terminal.addDistributorLinks(d);
-    }
-    
-    public void setCollector(EventCollector c) {
-        display.setCollector(c); 
-        terminal.setCollector(c);
-    }
-    
+				}, Clock.getStartDate(), 0, 5);
 
-    /**
+	}
+
+	public void setDistributor(EventDistributor d) {
+
+		// The clock device is connected to the EventDistributor here, even
+		// though the clock object is not constructed here,
+		// as no distributor is available to the Clock constructor.
+		Clock.getInstance().addDistributorLinks(d);
+		terminal.addDistributorLinks(d);
+	}
+	
+	public void setCollector(EventCollector c) {
+		display.setCollector(c);
+		terminal.setCollector(c);
+	}
+
+	/**
      * 
      */
-    @Override
-    public void addDStation(
-            String instanceName, 
-            int eastPos, 
-            int northPos,
-            int numPoints) {
-        logger.fine("");
-        
-        DStation newDStation = 
-                new DStation(instanceName, eastPos, northPos, numPoints);
-        dockingStationMap.put(instanceName, newDStation);
-        
-        // Now connect up DStation to event distributor and collector.
-        
-        EventDistributor d = terminal.getDistributor();
-        EventCollector c = display.getCollector();
-        
-        newDStation.setDistributor(d);
-        newDStation.setCollector(c);
-    }
-    
-    public DStation getDStation(String instanceName) {
-        return dockingStationMap.get(instanceName);
-    }
- 
+	@Override
+	public void addDStation(String instanceName, int eastPos, int northPos, int numPoints) {
+		logger.fine("");
 
+		DStation newDStation = new DStation(instanceName, eastPos, northPos,numPoints);
+		dockingStationMap.put(instanceName, newDStation);
+
+		// Now connect up DStation to event distributor and collector.
+
+		EventDistributor d = terminal.getDistributor();
+		EventCollector c = display.getCollector();
+
+		newDStation.setDistributor(d);
+		newDStation.setCollector(c);
+	}
+
+	public DStation getDStation(String instanceName) {
+		return dockingStationMap.get(instanceName);
+	}
+
+	
 }
