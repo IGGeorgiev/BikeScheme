82152c64c6225c4240bb47d952a62fae2cba25d7
Ivan Georgiev <ivangeorgiev95@gmail.com>
2015-11-28 17:03:50 +0000

    Finished User class.

--- BikeScheme/src/bikescheme/User.java
+++ BikeScheme/src/bikescheme/User.java
@@ -4,6 +4,36 @@ public class User {
     
     private String personalDetails;
     private String cardDetails;
+    private String keyId;
     
+    public User(String keyId,String personalDetails,String cardDetails){
+        this.setKeyId(keyId);
+        this.setCardDetails(cardDetails);
+        this.setPersonalDetails(personalDetails);
+    }
+
+    public String getCardDetails() {
+        return cardDetails;
+    }
+
+    public void setCardDetails(String cardDetails) {
+        this.cardDetails = cardDetails;
+    }
+
+    public String getPersonalDetails() {
+        return personalDetails;
+    }
+
+    public void setPersonalDetails(String personalDetails) {
+        this.personalDetails = personalDetails;
+    }
+
+    public String getKeyId() {
+        return keyId;
+    }
+
+    public void setKeyId(String keyId) {
+        this.keyId = keyId;
+    }
     
 }
