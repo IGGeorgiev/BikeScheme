8e1a538c595d4cd06bb6881b6615a44aa8c37077
ivoGen <ivaylo.genev95@gmail.com>
2015-12-01 09:13:54 +0000

    Implemented a UTC standard time in static field of Clock
    thereby fixing clearing bug

--- BikeScheme/src/bikescheme/SystemTest.java
+++ BikeScheme/src/bikescheme/SystemTest.java
@@ -65,16 +65,18 @@ public class SystemTest {
         expect("2 23:59, DSTouchScreen, A.ts, viewUserActivity, ordered-tuples, 4 ," 
                 + "HireTime, HireDS, ReturnDS, Duration (min),"
                 + "23:30   ,   B   ,    A    ,        25     ");
-        input("2 00:00, Clock, clk, tick");
-        expect("2 00:00, HubDisplay, hd, viewOccupancy, unordered-tuples, 6,"
+        input("3 00:00, Clock, clk, tick");
+        //CLEARING OCCURS HERE
+        expect("3 00:00, HubDisplay, hd, viewOccupancy, unordered-tuples, 6,"
              + "DSName, East, North, Status, #Occupied, #DPoints,"
              + "     A,    0,     0,     OK,         4,        5," 
              + "     B,  400,   300,    LOW,         0,        3");
         input ("3 00:01, DSTouchScreen, A.ts, viewActivity");
         expect("3 00:01, DSTouchScreen, A.ts, viewPrompt  , Please insert key.");
-        input ("3 00:01, KeyReader    , A.kr, keyInsertion, B.ki-1");
-        expect("3 00:01, DSTouchScreen, A.ts, viewUserActivity, ordered-tuples, 4 ," 
-                + "HireTime, HireDS, ReturnDS, Duration (min),");
+        input ("3 00:02, KeyReader    , A.kr, keyInsertion, B.ki-1");
+        expect("3 00:02, DSTouchScreen, A.ts, viewUserActivity, ordered-tuples, 4 ," 
+                + "HireTime, HireDS, ReturnDS, Duration (min),"
+                );
         
        // setupBikeConfig
     }
@@ -179,6 +181,7 @@ public class SystemTest {
                "     1,      1,                       500.00,                      25.00");
 
     }
+   
     /**
      * 
      * Setup demonstration system configuration:
